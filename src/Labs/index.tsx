a1b79aa3a87f0394d06d04ecc4ec4303dad900ad
SprialCloud <lxy990506@gmail.com>
2024-09-19 21:36:41 -0400

    modified

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,7 +8,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>Labs</h1>
+      <h1>Labs From Xinyu Liu(Section1)</h1>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
